005b0f3b70e0bbdcab86010e68b028dbb75dafc2
Jaron Heard <jaronheard@gmail.com>
2019-12-29 15:14:20 -0800

    add transitions and info page

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 import { Howl } from "howler"
 import { useStaticQuery, graphql } from "gatsby"
-import AniLink from "gatsby-plugin-transition-link/AniLink"
+import Info from "./info"
 
 const defaultHowl = {
   autoplay: false,
@@ -17,15 +17,15 @@ const createHowl = src => {
 }
 const buttonStyle = {
   fontFamily: `'Roboto Condensed', sans-serif`,
-  width: `170px`,
-  paddingTop: `30px`,
-  paddingBottom: `30px`,
+  width: `85px`,
+  paddingTop: `10px`,
+  paddingBottom: `10px`,
   textAlign: `center`,
   color: `#000000`,
   textTransform: `uppercase`,
   fontWeight: `600`,
-  marginLeft: `30px`,
-  marginBottom: `30px`,
+  marginLeft: `10px`,
+  marginBottom: `10px`,
   cursor: `pointer`,
   display: `inline-block`,
   backgroundColor: `transparent`,
@@ -50,6 +50,7 @@ const Audio = () => {
   const [index, setIndex] = useState(0)
   const [playing, setPlaying] = useState(false)
   const [track, setTrack] = useState(null)
+  const [info, setInfo] = useState(false)
   const [allHowls] = useState(
     data.allFile.edges.map(file => createHowl(file.node.publicURL))
   )
@@ -68,7 +69,7 @@ const Audio = () => {
   const stopAudio = audio => {
     try {
       const howlObj = audio
-      howlObj.fade(howlObj.volume(), 0, 750, track)
+      howlObj.fade(howlObj.volume(), 0, 1000, track)
     } catch (error) {
       // eslint-disable-next-line
       console.warn("Unknown SFX theme requested to stop ")
@@ -82,32 +83,36 @@ const Audio = () => {
         minHeight: `100vh`,
         padding: `1.45rem 1.0875rem`,
         backgroundColor: `#${data.allFile.edges[index].node.name}`,
+        transition: `all 1000ms ease-in-out`,
       }}
     >
-      <AniLink fade to="info">
-        <button style={buttonStyle}>info</button>
-      </AniLink>
-      <button
-        style={buttonStyle}
-        onClick={() => {
-          playing ? stopAudio(allHowls[index]) : playAudio(allHowls[index])
-          setPlaying(!playing)
-        }}
-      >
-        {playing ? "Stop" : "Play"}
-      </button>
-      <button
-        style={buttonStyle}
-        onClick={() => {
-          const nextIndex =
-            index + 1 === data.allFile.edges.length ? 0 : index + 1
-          playing && stopAudio(allHowls[index])
-          playing && playAudio(allHowls[nextIndex])
-          setIndex(nextIndex)
-        }}
-      >
-        Next
-      </button>
+      <Info in={info} />
+      <div style={{ margin: `0 auto`, width: `max-content` }}>
+        <button style={buttonStyle} onClick={() => setInfo(!info)}>
+          info
+        </button>
+        <button
+          style={buttonStyle}
+          onClick={() => {
+            playing ? stopAudio(allHowls[index]) : playAudio(allHowls[index])
+            setPlaying(!playing)
+          }}
+        >
+          {playing ? "Stop" : "Play"}
+        </button>
+        <button
+          style={buttonStyle}
+          onClick={() => {
+            const nextIndex =
+              index + 1 === data.allFile.edges.length ? 0 : index + 1
+            playing && stopAudio(allHowls[index])
+            playing && playAudio(allHowls[nextIndex])
+            setIndex(nextIndex)
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/info.js
@@ -0,0 +1,64 @@
+import React from "react"
+
+import { Transition } from "react-transition-group"
+
+const duration = 1000
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+}
+
+const infoStyle = {
+  fontFamily: `'Roboto Condensed', sans-serif`,
+  position: `absolute`,
+  top: `0`,
+  left: `0`,
+  width: `100%`,
+  height: `calc(100% - 6em)`,
+  zIndex: `10`,
+  padding: `1em`,
+  marginTop: `6em`,
+  background: `transparent`,
+}
+
+const Info = ({ in: inProp }) => (
+  <Transition in={inProp} timeout={duration}>
+    {state => (
+      <div
+        style={{
+          ...defaultStyle,
+          ...transitionStyles[state],
+        }}
+      >
+        <div style={infoStyle}>
+          <div style={{ margin: `0 auto`, maxWidth: `800px` }}>
+            <p>
+              Burmese abyssinian . Malkin balinese , yet thai, and russian blue.
+              American bobtail balinese . Himalayan. Turkish angora. Himalayan
+              american bobtail. Scottish fold balinese scottish fold maine coon
+              for american bobtail but mouser.
+            </p>
+            <p>
+              Panther ragdoll puma or panther. Siberian tom but egyptian mau.
+              Singapura jaguar so jaguar yet american shorthair ocelot so
+              cornish rex. Siamese american shorthair and scottish fold ocelot.
+            </p>
+            <p>
+              <a href="https://vektroid.bandcamp.com/album/vektroid-texture-maps">Sounds</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    )}
+  </Transition>
+)
+
+export default Info
