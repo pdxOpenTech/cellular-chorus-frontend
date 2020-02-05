0897f1a2d6fac2848879a58a4620eec91403faf4
Patricia Wolf <pwolf@Patricias-MacBook-Air.local>
2020-02-05 10:22:33 -0800

    edited the artist statement

--- src/components/info.js
+++ src/components/info.js
@@ -46,7 +46,7 @@ const Info = ({ in: inProp }) => (
             <p>
             Cellular Chorus is a work of spatialized aleatoric music using 
             smartphones to bring people physically closer to have an 
-            interactive and collective experience with music.
+            interactive and collective experience with light and sound.
             </p>
             <p>
             The piece is played by each user visiting cellularchorus.com on 
@@ -55,7 +55,7 @@ const Info = ({ in: inProp }) => (
             a random sound clip will be ready to be played. Each sound comes 
             with a unique color I have chosen to pair with the sound. In the 
             dark, the room will glow with the colored lights on the screen. 
-            Press play to hear it. If you want to hear another sound, press 
+            Press play to hear a sound. If you want to hear another sound, press 
             the “Next” button. The user has control of the volume through their 
             phone’s normal volume control buttons. As others play the sounds in 
             the space, listen to how they work together. Mix your sounds in and 
@@ -65,9 +65,9 @@ const Info = ({ in: inProp }) => (
             the sounds on the site or let a favorite one loop forever as the others 
             around you change unpredictably. The sounds I made are meant to 
             harmonize. There is no right or wrong way to play it. The intention of 
-            this piece is to use your smartphone for deep listening, creative 
-            experimentation, and immersing in a sound and light environment within 
-            a tangible space with real face to face interactions. 
+            this piece is to let you use your smartphone for deep listening, creative 
+            experimentation, and to immerse groups into a sound and light environment 
+            within a tangible space with real face to face interaction. 
             </p>
             <p>
               Concept and sound design by Patricia Wolf.
